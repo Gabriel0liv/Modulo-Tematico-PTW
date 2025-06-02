12de1d72ae579beee9781227a6f3b11e3ec0746f
Gabriell0liv <gabrielr.oliveira@ua.pt>
2025-06-02 16:12:59 +0100

    Adiciona relacionamentos de comprador e produtos ao modelo User; atualiza ProdutoController para destacar produtos e exibir imagens corretamente

--- GameSwap/app/Models/User.php
+++ GameSwap/app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\TipoUer;
-use App\Produto;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
