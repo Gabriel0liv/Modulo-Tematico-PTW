0f47449d7e958ce97a2973aefd752b436149d7ea
Jao <you@example.com>
2025-06-28 14:22:43 +0000

    preparação para deploy

--- GameSwap/app/Notifications/EmailBanimentoConta.php
+++ GameSwap/app/Notifications/EmailBanimentoConta.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EmailBanimentoConta extends Notification
+class EmailBanimentoConta extends Notification implements ShouldQueue
 {
     public function via($notifiable)
     {
